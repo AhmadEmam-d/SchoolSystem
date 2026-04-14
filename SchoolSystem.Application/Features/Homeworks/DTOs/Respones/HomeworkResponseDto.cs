6ac9db95b41513c81ad6c64093f8c25c181bd256
ali kamal <ali123kamal12@gmail.com>
2026-04-14 19:14:29 +0200

    .

---
+++ SchoolSystem.Application/Features/Homeworks/DTOs/Respones/HomeworkResponseDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SchoolSystem.Application.Features.Homeworks.DTOs.Respones
+{
+    public class HomeworkResponseDto
+    {
+        public Guid Oid { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string ClassName { get; set; } = string.Empty;
+        public DateTime DueDate { get; set; }
+    }
+}
