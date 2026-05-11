c362afce7cba85934219d4fdf66618bc70bf0e1d
Ahmad emam <ae610226@gmail.com>
2026-05-11 14:23:00 +0300

    fix delete exam

--- SchoolSystem.Application/Features/Exams/Commands/Delete/DeleteExamCommandHandler.cs
+++ SchoolSystem.Application/Features/Exams/Commands/Delete/DeleteExamCommandHandler.cs
@@ -1,22 +1,50 @@
 ﻿using MediatR;
+using Microsoft.EntityFrameworkCore;
 using SchoolSystem.Domain.Entities;
 using SchoolSystem.Domain.Interfaces.Common;
-using System.Threading;
-using System.Threading.Tasks;
 
 namespace SchoolSystem.Application.Features.Exams.Commands.Delete
 {
     public class DeleteExamCommandHandler : IRequestHandler<DeleteExamCommand, bool>
     {
         private readonly IGenericRepository<Exam> _examRepo;
+        private readonly IGenericRepository<ExamResult> _examResultRepo;
+        private readonly IGenericRepository<Material> _materialRepo;
 
-        public DeleteExamCommandHandler(IGenericRepository<Exam> examRepo)
+        public DeleteExamCommandHandler(
+            IGenericRepository<Exam> examRepo,
+            IGenericRepository<ExamResult> examResultRepo,
+            IGenericRepository<Material> materialRepo)
         {
             _examRepo = examRepo;
+            _examResultRepo = examResultRepo;
+            _materialRepo = materialRepo;
         }
 
         public async Task<bool> Handle(DeleteExamCommand request, CancellationToken cancellationToken)
         {
+            var exam = await _examRepo.GetByOidAsync(request.Oid);
+            if (exam is null)
+                return false;
+
+            // Delete related ExamResults
+            var results = await _examResultRepo
+                .GetAllQueryable()
+                .Where(r => r.ExamOid == request.Oid)
+                .ToListAsync(cancellationToken);
+
+            foreach (var result in results)
+                await _examResultRepo.DeleteAsync(result.Oid);
+
+            // Delete related Materials
+            var materials = await _materialRepo
+                .GetAllQueryable()
+                .Where(m => m.ExamOid == request.Oid)
+                .ToListAsync(cancellationToken);
+
+            foreach (var material in materials)
+                await _materialRepo.DeleteAsync(material.Oid);
+
             await _examRepo.DeleteAsync(request.Oid);
             return true;
         }
